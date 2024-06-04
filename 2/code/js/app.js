daca77faa322a6a66f7cf6e62bdc5de233c0dc20
DongHyun05 <bulletseu@gmail.com>
2024-06-04 15:47:40 +0900

    20240604

--- 2/code/js/app.js
+++ 2/code/js/app.js
@@ -40,3 +40,18 @@ jQuery(document).ready(function ($) {
   }
   titleAnim();
 });
+
+let swiper = new Swiper(".container", {
+  slidePerView: 3,
+  spaceBetween: 30,
+  grabCursor: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
