10dbf58a7e9a4c2a115a02ea9313e886504ea1a9
DongHyun05 <bulletseu@gmail.com>
2024-06-10 11:01:25 +0900

    20240610

--- 2/code/js/app.js
+++ 2/code/js/app.js
@@ -103,3 +103,33 @@ new Chart(document.getElementById("bar-chart"), {
     },
   },
 });
+
+$(window).ready(function () {
+  draw(41.5, ".pie-chart1", "#ccc");
+  draw(41.1, ".pie-chart2", "#8b22ff");
+  draw(9.7, ".pie-chart3", "#ff0");
+});
+
+function draw(max, classname, colorname) {
+  var i = 1;
+  var func1 = setInterval(function () {
+    if (i < max) {
+      color1(i, classname, colorname);
+      i++;
+    } else {
+      clearInterval(func1);
+    }
+  }, 10);
+}
+function color1(i, classname, colorname) {
+  $(classname).css({
+    background:
+      "conic-gradient(" +
+      colorname +
+      " 0% " +
+      i +
+      "%, #ffffff " +
+      i +
+      "% 100%)",
+  });
+}
